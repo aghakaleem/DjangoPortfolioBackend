2084b5163c52c0770557535b46a37a9bf5e09ed4
agha_kaleem <aghakaleemullah.bscssef20@iba-suk.edu.pk>
2025-05-20 22:09:59 +0500

    Added some models in blog app

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,64 @@
 from django.db import models
-
+from django.utils.text import slugify
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class CustomUser(models.Model):
+    full_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    profile_picture_url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.email
+    
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+            unique_slug = self.slug
+            counter = 1
+            while Post.objects.filter(slug=unique_slug).exists():
+                unique_slug = f"{self.slug}-{counter}"
+                counter += 1
+            self.slug = unique_slug      
+        super().save(*args, **kwargs)
+
+class PostSection(models.Model):
+    SECTION_TYPE_CHOICES = (
+        ('text', 'Text'),
+        ('image', 'Image'),
+    )
+
+    post = models.ForeignKey(Post, related_name="sections", on_delete=models.CASCADE)
+    order = models.PositiveIntegerField()
+    section_type = models.CharField(max_length=10, choices=SECTION_TYPE_CHOICES)
+    text = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to=f"{post.slug}/sections_img/", blank=True, null=True)
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return f"{self.post.title} - {self.get_section_type_display()} [{self.order}]"
+    
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"Comment by {self.user.email} on {self.post.title}"
--- PortfolioBackend/settings.py
+++ PortfolioBackend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Api',
+    'Blog',
     'rest_framework',
 ]
 
@@ -129,3 +130,4 @@ MEDIA_ROOT=BASE_DIR/'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'Blog.CustomUser'
